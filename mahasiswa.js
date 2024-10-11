0594ae8c4e1c046c9d4428eae96d4e2f65f2408d
annafary <annfarysia@gmail.com>
2024-10-11 14:46:04 +0700

    Remaja Muda.js

--- mahasiswa.js
+++ mahasiswa.js
@@ -20,54 +20,10 @@ class Remaja Muda {
         this._umur = value; 
 
     perkenalan() {
-        return `hallo, perkenalkan saya ${this._nama} dan umur saya ${this._umur} tahun.`; // Method yang bisa dioverride
+        return `hallo, perkenalkan saya ${this._nama} dan umur saya ${this._umur} tahun.`; 
     }
 }
-
-
-class Mahasiswa extends Remaja Muda {
-    constructor(nama, umur, jurusan) {
-        super(nama, umur);
-        this._jurusan = jurusan; 
-    }
-
-    get jurusan() {
-        return this._jurusan;
-    }
-
-    set jurusan(value) {
-        this._jurusan = value; 
-    }
-
-    perkenalan() {
-        return `${super.perkenalan()} saya menempuh pendidikan ${this._jurusan}.`; 
-    }
-}
-
-
-class Mahasiswa2 extends Mahasiswa {
-    constructor(nama, umur, jurusan) {
-        super(nama, umur);
-        this._position = position; 
-    }
-
-    get jurusan() {
-        return this._jurusan;
-    }
-
-    set jurusan(value) {
-        this._jurusann = value; 
-    }
-
-    perkenalan() {
-        return `${super.perkenalan()} saya menempuh pendidikan ${this._jurusan}.`; 
-    }
-}
-
-
-class Mahasiswa3 extends Mahasiswa2 {
-    constructor(nama, umur, jurursan) {
-        super(nama, umur, 'Mahasiswa3');
+umur, 'Mahasiswa3');
         this._jurusan = jurusan; 
     }
 
@@ -84,19 +40,3 @@ class Mahasiswa3 extends Mahasiswa2 {
     }
 }
 
-
-class Main {
-    static main() {
-        const mahasiswa = new mahasiswa("Anna", 19, "Informatika");
-        const mahasiswa2 = new mahasiswa2("tata", 20, "Industri");
-        const mahasiswa3 = new mahasiswa3("mutiara", 17, "Sastra");
-
-        console.log(mahasiswa.perkenlan());
-        console.log(mahasiswa2.perkenlan());
-        console.log(mahasiswa3.perkenlan());
-    }
-}
-
-
-Main.main();
-node app.js
