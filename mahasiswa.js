2ff229fca2d271c651015fe420c991a4410b0856
annafary <annfarysia@gmail.com>
2024-10-11 14:55:45 +0700

    Update mahasiswa.js

--- mahasiswa.js
+++ mahasiswa.js
@@ -23,20 +23,3 @@ class Remaja Muda {
         return `hallo, perkenalkan saya ${this._nama} dan umur saya ${this._umur} tahun.`; 
     }
 }
-umur, 'Mahasiswa3');
-        this._jurusan = jurusan; 
-    }
-
-    get jurusan() {
-        return this._jurusan; 
-    }
-
-    set jurusan(value) {
-        this._jurusan = value;
-    }
-
-    perkenalan() {
-        return `${super.perkenalan()} saya menempuh pendidikan ${this._jurusan}.`; 
-    }
-}
-
