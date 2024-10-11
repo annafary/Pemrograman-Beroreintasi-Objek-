2cc14681cbe2dd5b87118d29f0ca395204a87865
annafary <annfarysia@gmail.com>
2024-10-11 14:49:39 +0700

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,14 @@
+class Main {
+    static main() {
+        const mahasiswa = new mahasiswa("Anna", 19, "Informatika");
+        const mahasiswa2 = new mahasiswa2("tata", 20, "Industri");
+        const mahasiswa3 = new mahasiswa3("mutiara", 17, "Sastra");
+
+        console.log(mahasiswa.perkenlan());
+        console.log(mahasiswa2.perkenlan());
+        console.log(mahasiswa3.perkenlan());
+    }
+}
+
+
+Main.main();
